27952fcff4da722ad90aa233210e0b5b5a0dd333
infinith4 <infinith4@gmail.com>
2023-04-19 19:27:53 +0900

    mod

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -5,23 +5,10 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
-    /**
-     * @dev
-     * - このコントラクトをデプロイしたアドレス用変数owner
-     */
-    address public owner;
 
-    constructor() ERC721("OnlyOwnerMint", "OWNER"){
-        owner = _msgSender();
-    }
+    constructor() ERC721("MyNFT", "MYNFT"){}
 
-    /**
-     * @dev
-     * - このコントラクトをデプロイしたアドレスだけがmint可能 require
-     * - nftMint 関数の実行アドレスにtokenIdを紐付け
-     */
-    function nftMint(uint256 tokenId) public {
-        require(owner == _msgSender(), "Caller is not the owner.");
-        _mint(_msgSender(), tokenId);
+    function nftMint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
     }
 }
---
+++ contracts/OnlyOwnerMint.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract OnlyOwnerMint is ERC721 {
+    /**
+     * @dev
+     * - このコントラクトをデプロイしたアドレス用変数owner
+     */
+    address public owner;
+
+    constructor() ERC721("OnlyOwnerMint", "OWNER"){
+        owner = _msgSender();
+    }
+
+    /**
+     * @dev
+     * - このコントラクトをデプロイしたアドレスだけがmint可能 require
+     * - nftMint 関数の実行アドレスにtokenIdを紐付け
+     */
+    function nftMint(uint256 tokenId) public {
+        require(owner == _msgSender(), "Caller is not the owner.");
+        _mint(_msgSender(), tokenId);
+    }
+}
